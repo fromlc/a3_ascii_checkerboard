352645549c0dddb1e89a93d0bf1345f76648d47f
L Carver <wifihum@gmail.com>
2023-10-23 12:31:00 -0700

    add function calls, message constant

--- ascii_checkerboard.cpp
+++ ascii_checkerboard.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 
 // comment to omit debug output
-#define _MYDEBUG
+//#define _MYDEBUG
 
 using namespace std;
 
@@ -16,8 +16,7 @@ using namespace std;
 constexpr int BOARD_ROWS = 8;
 constexpr int BOARD_COLS = 8;
 
-constexpr char CH_DARK = 'X';
-constexpr char CH_LITE = 'O';
+const string MSG_SQUARE_COLOR = " square character? ";
 
 //------------------------------------------------------------------------------
 // globals
@@ -25,12 +24,17 @@ constexpr char CH_LITE = 'O';
 namespace cb {
 
 	// #TODO will this line init entire array??
-	char ac2d_checkerboard[BOARD_ROWS][BOARD_COLS]{ '.' };
+	char ac2d_checkerboard[BOARD_ROWS][BOARD_COLS] { '.' };
+	char lite { 'O' };
+	char dark { 'X' };
 }
 
 //------------------------------------------------------------------------------
 // local function prototypes
 //------------------------------------------------------------------------------
+void startup();
+char get_user_char(const string&);
+void prep_2d_array();
 void display_2d_array();
 
 //------------------------------------------------------------------------------
@@ -38,32 +42,64 @@ void display_2d_array();
 //------------------------------------------------------------------------------
 int main() {
 
-	// set each array element to known value
+	startup();
+
+	prep_2d_array();
+
+	display_2d_array();
+
+	return 0;
+}
+
+//------------------------------------------------------------------------------
+// display app banner, prompt for light and dark chars
+//------------------------------------------------------------------------------
+void startup() {
+
+	// app banner
+	cout << '\n' << BOARD_ROWS << 'x' << BOARD_ROWS << " Checkerboard\n\n";
+
+	// prompt for light and dark square chars
+	cb::lite = get_user_char("Light");
+	cb::dark = get_user_char("Dark");
+}
+
+//------------------------------------------------------------------------------
+// get char value from user
+//------------------------------------------------------------------------------
+char get_user_char(const string& msg) {
+
+	cout << msg << MSG_SQUARE_COLOR;
+	char color;
+	cin >> color;
+
+	return color;
+}
+
+//------------------------------------------------------------------------------
+// init 2d array with checkerboard chars
+//------------------------------------------------------------------------------
+void prep_2d_array() {
+
+	// set all array elements to light square value
 	int num_bytes = BOARD_ROWS * BOARD_COLS;
-	memset(cb::ac2d_checkerboard, CH_LITE, num_bytes);
+	memset(cb::ac2d_checkerboard, cb::lite, num_bytes);
 
 #ifdef _MYDEBUG
 	// display array contents after memset
 	display_2d_array();
 #endif
 
-	// build array of lite and dark squares
+	// set dark square elements
 	for (int row = 0; row < BOARD_ROWS; ++row) {
-
 		for (int col = 0; col < BOARD_ROWS; ++col) {
 
 			// row even and col odd OR row odd and col even => DARK
 			//if (row % 2 == 0 && col % 2 == 1 || row % 2 == 1 && col % 2 == 0)
 			if (row % 2 != col % 2)
-				cb::ac2d_checkerboard[row][col] = CH_DARK;
+				cb::ac2d_checkerboard[row][col] = cb::dark;
 		}
 	}
-
-	// display array contents after checkerboarding
-	display_2d_array();
-
-	return 0;
-
 }
 
 //------------------------------------------------------------------------------
@@ -71,11 +107,13 @@ int main() {
 //------------------------------------------------------------------------------
 void display_2d_array() {
 
+	cout << '\n';
+
 	for (int row = 0; row < BOARD_ROWS; ++row) {
 		for (int col = 0; col < BOARD_COLS; ++col) {
 			cout << cb::ac2d_checkerboard[row][col];
 		}
 		cout << '\n';
 	}
-	cout << "\n\n";
+	cout << '\n';
 }
